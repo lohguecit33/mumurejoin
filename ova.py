7a8439aeb51e8ecbb4ee2bf7187888025f8eab41
bang ova <botagun3@gmail.com>
2025-01-20 22:10:41 +0700

    Update ova.py

--- ova.py
+++ ova.py
@@ -112,9 +112,13 @@ def check_internet_connection(device_id):
 def start_private_server(device_id, private_link):
     try:
         subprocess.run(
-            [ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-a', 'android.intent.action.VIEW', '-d', private_link],
+            [ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-n', 'com.roblox.client/com.roblox.client.startup.ActivitySplash', '-d', private_link],
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(8)
+        # Membuka game di Roblox setelah ActivitySplash
+        subprocess.run([ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-n', 'com.roblox.client/com.roblox.client.ActivityProtocolLaunch', '-d', private_link],
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(8)                               
         print(colored(f"Private link dijalankan di emulator {device_id}.", "green"))
     except Exception as e:
         print(colored(f"Gagal menjalankan Private Server di emulator {device_id}: {e}", "red"))
@@ -124,9 +128,13 @@ def start_default_server(device_id, game_id):
     try:
         game_url = f"roblox://placeID={game_id}"
         subprocess.run(
-            [ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-a', 'android.intent.action.VIEW', '-d', game_url],
+            [ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-n', 'com.roblox.client/com.roblox.client.startup.ActivitySplash', '-d', game_url],
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(8)
+        # Membuka game di Roblox setelah ActivitySplash
+        subprocess.run([ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-n', 'com.roblox.client/com.roblox.client.ActivityProtocolLaunch', '-d', game_url],
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        print(Fore.GREEN + f"Membuka game menggunakan server: {game_url}.")       
     except Exception as e:
         print(colored(f"Failed to start Default Server: {e}", 'red'))
 
