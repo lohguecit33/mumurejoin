301d719ac2ab6b244ac76a1bf9d5fdcc02ba05b4
bang ova <botagun3@gmail.com>
2025-07-01 20:55:59 +0700

    Update ova.py

--- ova.py
+++ ova.py
@@ -91,12 +91,8 @@ def start_private_server(device_id, private_link):
             [ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-n', 'com.roblox.client/com.roblox.client.startup.ActivitySplash', '-d', private_link],
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(5)
-        subprocess.run([
-            ADB_PATH, '-s', f'127.0.0.1:{device_id}', 
-            'shell', 'am', 'start', 
-            '-n', 'com.roblox.client/com.roblox.client.ActivityGame', 
-            '-d', private_link
-        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run([ADB_PATH, '-s', f'127.0.0.1:{device_id}', 'shell', 'am', 'start', '-n', 'com.roblox.client/com.roblox.client.ActivityProtocolLaunch', '-d', private_link],
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(5)                               
         print(colored(f"Private link dijalankan di emulator {device_id}.", "green"))
     except Exception as e:
